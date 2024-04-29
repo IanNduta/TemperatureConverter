6a915796d79f0082dacae3d403ecda4691c1daf4
Ian Nduta <gathonjiandutai@gmail.com>
2024-04-29 11:36:07 -0400

    created js files for the conversions

---
+++ scripts/fahrenheit.js
@@ -0,0 +1,11 @@
+"use script";
+window.onload;
+
+var fahrenheit;
+var celsius = 80;
+
+fahrenheit = (celsius * 9/5) + 32;
+
+var message = celsius + "C is " + fahrenheit + "F";
+
+console.log(message);
